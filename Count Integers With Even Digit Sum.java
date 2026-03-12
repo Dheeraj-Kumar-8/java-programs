3f04a9e42267d8cf7afd90183a1315d303f00381
Dheeraj Kumar <dheerajdasari88@gmail.com>
2026-03-12 19:56:43 +0530

    Method to count integers with even digit sum
    Given a positive integer num, return the number of positive integers less than or equal to num whose digit sums are even.
    
    The digit sum of a positive integer is the sum of all its digits.

---
+++ Count Integers With Even Digit Sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countEven(int num) {
+        int count = 0;
+        for(int i=1;i<=num;i++)
+        {
+            int sum = 0;
+            int temp = i;
+            while(temp>0)
+            {
+                int r  = temp%10;
+                sum+=r;
+                temp = temp/10;
+                
+            }
+            if(sum%2==0)
+            {
+                count++;
+            }
+        }
+        return count;
+        
+    }
+}
